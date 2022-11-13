07db2c127b029a9cc4720844cc379145b2035fb4
bigfish <377342130@qq.com>
2022-11-13 18:48:43 +0800

    fix: fixed vitepress build error: window is not define

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,22 +1,54 @@
 import { defineConfig } from "vitepress";
+import { LIGHT_THEME_CONFIG, DARK_THEME_CONFIG } from "./constants";
 export default defineConfig({
-    title: 'Mr7ish',
-    description: 'Just playing around.',
-    // outline: 0,
-    themeConfig: {
-        socialLinks: [
-            { icon: 'github', link: 'https://github.com/mr7ish' },
-        ],
-        nav: [
-            { text: '首页', link: '/' },
-            { text: 'Gitee', link: 'https://gitee.com/chengguanmo' },
-        ],
-    },
-    head: [
-        ['script', { src: 'https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js', crossorigin: '', onload:'initCanvas()' }]
-    ]
-    
-    // base:'/mr7ish'
-    // outDir: '../'
-})
-// 
+  title: "Mr7ish",
+  description: "Just playing around.",
+  // outline: 0,
+  themeConfig: {
+    socialLinks: [{ icon: "github", link: "https://github.com/mr7ish" }],
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "Gitee", link: "https://gitee.com/chengguanmo" },
+    ],
+  },
+  head: [
+    [
+      "script",
+      {},
+      `const div = document.createElement("div");
+    div.style.cssText = 'position: fixed; top:0; left:0; width: 100vw; height: 100vh; z-index: 0;';
+    div.id = "particlesJS_canvas";
+    document.body.append(div);
+    const init = () => {
+      if (window.particlesJS) {
+        window.particlesJS(
+          "particlesJS_canvas",
+          
+          JSON.parse(
+            document.querySelector("html.dark") ? '${JSON.stringify(DARK_THEME_CONFIG)}'
+            : '${JSON.stringify(LIGHT_THEME_CONFIG)}')
+        ); 
+      }
+    };
+    window.initCanvas = init;
+
+    const originFn = window.localStorage.setItem;
+    window.localStorage.setItem = (key, value) => {
+      init();
+      originFn.apply(window.localStorage, [key, value]);
+    };`,
+    ],
+    [
+      "script",
+      {
+        src: "https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js",
+        crossorigin: "",
+        onload: "initCanvas()",
+      },
+    ],
+  ],
+
+  // base:'/mr7ish'
+  // outDir: '../'
+});
+//
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,45 +1,12 @@
-import defaultTheme from 'vitepress/theme'
-import { EnhanceAppContext } from 'vitepress'
-import Tag from '../components/Tag.vue'
-import '../styles/index.css'
-
-import { LIGHT_THEME_CONFIG, DARK_THEME_CONFIG } from '../constants'
-
+import defaultTheme from "vitepress/theme";
+import { EnhanceAppContext } from "vitepress";
+import Tag from "../components/Tag.vue";
+import "../styles/index.css";
 
 export default {
-    ...defaultTheme,
-    enhanceApp: (ctx: EnhanceAppContext) => {
-        defaultTheme.enhanceApp(ctx)
-        ctx.app.component('Tag', Tag);
-        if (window && typeof window !== undefined) {
-            const div = document.createElement('div')
-            div.style.cssText = `
-                position: fixed;
-                top:0;
-                left:0;
-                width: 100vw;
-                height: 100vh;
-                z-index: 0;
-            `
-            div.id = 'particlesJS_canvas'
-            document.body.append(div)
-            const init = () => {
-                if (window.particlesJS) {
-                    console.log(DARK_THEME_CONFIG);
-                    window.particlesJS('particlesJS_canvas', document.querySelector('html.dark') ? DARK_THEME_CONFIG : LIGHT_THEME_CONFIG);
-                }
-            }
-            window.initCanvas = init
-
-            const originFn = window.localStorage.setItem
-            window.localStorage.setItem = (key, value) => {
-                init()
-                originFn.apply(window.localStorage, [key, value])
-            }
-            // const targetNode = document.getElementsByTagName('html')[0]
-            // const observer = new MutationObserver(init)
-            // const config = { attributes: true };
-            // observer.observe(targetNode, config)
-        }
-    }
-}
+  ...defaultTheme,
+  enhanceApp: (ctx: EnhanceAppContext) => {
+    defaultTheme.enhanceApp(ctx);
+    ctx.app.component("Tag", Tag);
+  }
+};
