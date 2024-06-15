0ffb76772442f10906b73d87e626ab9f4c941412
bigfish <377342130@qq.com>
2024-06-15 23:54:02 +0800

    feat: could back top

--- .vitepress/theme/layout/index.vue
+++ .vitepress/theme/layout/index.vue
@@ -3,17 +3,28 @@
     <template #doc-before>
       <DocBefore />
     </template>
+    <template #doc-after>
+      <div
+        v-if="isNeedBackTop"
+        @click="backTop"
+        class="top"
+      >
+        top
+      </div>
+    </template>
   </Layout>
 </template>
 
 <script setup lang="ts">
 import DefaultTheme from "vitepress/theme";
 import DocBefore from "../components/DocBefore.vue";
-import { onMounted, ref } from "vue";
+import { nextTick, onMounted, onUnmounted, ref } from "vue";
 
 const { Layout } = DefaultTheme;
 
 const content = ref("");
+
+const isNeedBackTop = ref(false);
 
 const getArticles = async () => {
   const modules = import.meta.glob("../../../site/articles/*.md", {
@@ -26,9 +37,45 @@ const getArticles = async () => {
   console.log("articles =>", articles);
 };
 
+const backTop = () => {
+  // document.documentElement.scrollTop = 0;
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+const calcScrollHeight = () => {
+  const scrollY = window.scrollY;
+
+  if (scrollY >= 1600) {
+    isNeedBackTop.value = true;
+  } else {
+    isNeedBackTop.value = false;
+  }
+  // console.log("scrollY =>", scrollY);
+};
+
 onMounted(() => {
   getArticles();
+  window.addEventListener("scroll", calcScrollHeight);
+});
+
+onUnmounted(() => {
+  window.addEventListener("scroll", calcScrollHeight);
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.top {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 40px;
+  height: 40px;
+  background-color: lightcoral;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  z-index: 11;
+}
+</style>
