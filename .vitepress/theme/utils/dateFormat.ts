09dc7c4ff1828375d7a6a0152b456f8a9332ac90
cgm <377342130@qq.com>
2024-08-01 17:01:34 +0800

    fix: adjust the variable name

--- .vitepress/theme/utils/dateFormat.ts
+++ .vitepress/theme/utils/dateFormat.ts
@@ -1,7 +1,7 @@
 import moment from "moment";
 
 export function getShorthandDate(date: string) {
-  const isCurrentYeat = moment(date).year() === moment(Date.now()).year();
-  const format = isCurrentYeat ? "MMM D" : "MMM D, YYYY";
+  const isCurrentYear = moment(date).year() === moment(Date.now()).year();
+  const format = isCurrentYear ? "MMM D" : "MMM D, YYYY";
   return moment(date).format(format);
 }
