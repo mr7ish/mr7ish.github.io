cff018ba13b9e52b8b009ee60ff7ddd593a459fe
cgm <377342130@qq.com>
2024-10-08 18:05:54 +0800

    fix: update download function

--- .vitepress/theme/utils/download.ts
+++ .vitepress/theme/utils/download.ts
@@ -12,9 +12,29 @@ export function createAnchor(href: string) {
 /**
  * download source
  */
-export function download(source: string, name?: string) {
-  const anchor = createAnchor(source);
-  anchor.download = name ?? "download";
-  anchor.click();
-  notice("download success", "success");
+// export function download(source: string, name?: string) {
+//   const anchor = createAnchor(source);
+//   anchor.download = name ?? "download";
+//   anchor.click();
+//   notice("download success", "success");
+// }
+
+export function download(imageUrl: string, fileName: string) {
+  fetch(imageUrl)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.style.display = "none";
+      a.href = url;
+      a.download = fileName;
+
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+
+      notice("download success", "success");
+    })
+    .catch((_error) => notice("download error", "error"));
 }
