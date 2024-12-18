2b39c144d7039386676dbe940236c6e417f54dd2
bigfish <377342130@qq.com>
2024-12-18 17:11:48 +0800

    feat: add LinkTo component

---
+++ .vitepress/theme/components/LinkTo.vue
@@ -0,0 +1,25 @@
+<template>
+  <a
+    class="link-to"
+    :href="'#' + text"
+    :title="text"
+  >
+    {{ text }}
+  </a>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  text?: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  text: "",
+});
+</script>
+
+<style scoped lang="less">
+.link-to {
+  color: #bd34fe;
+}
+</style>
