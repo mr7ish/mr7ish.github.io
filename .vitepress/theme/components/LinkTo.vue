9a2b67603b924bf653c78d53975428b8a652087a
bigfish <377342130@qq.com>
2025-04-17 15:33:16 +0800

    fix: update LinkTo component

--- .vitepress/theme/components/LinkTo.vue
+++ .vitepress/theme/components/LinkTo.vue
@@ -1,25 +1,37 @@
 <template>
   <a
-    class="link-to"
-    :href="'#' + text"
-    :title="text"
+    :class="isAnchor ? 'anchor-link' : 'vitepress-link'"
+    :href="isAnchor ? '#' + href : href"
+    :target="isAnchor ? undefined : '_blank'"
+    :rel="isAnchor ? undefined : 'noreferrer'"
+    :title="href"
   >
-    {{ text }}
+    <slot>{{ href }}</slot>
   </a>
 </template>
 
 <script setup lang="ts">
 type Props = {
-  text?: string;
+  href?: string;
+  isAnchor?: boolean;
 };
 
 const props = withDefaults(defineProps<Props>(), {
-  text: "",
+  href: "",
+  isAnchor: true,
 });
 </script>
 
 <style scoped lang="less">
-.link-to {
+.vitepress-link {
+  font-weight: 500;
+  color: var(--vp-c-brand-1);
+  text-decoration: underline;
+  text-underline-offset: 2px;
+  transition: color 0.25s, opacity 0.25s;
+}
+
+.anchor-link {
   color: #bd34fe;
 }
 </style>
